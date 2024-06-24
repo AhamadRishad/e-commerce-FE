6e58ef8ef3019c0e3788790b6430b954c30a1dd3
Ahamad Rishad <aahamadrishad@gmail.com>
2024-06-24 06:22:04 +0530

    vertical horizontal card dark mode modified

--- src/components/VerticalCardProducts.jsx
+++ src/components/VerticalCardProducts.jsx
@@ -52,7 +52,7 @@ const VerticalCardProducts = ({ category, heading }) => {
     <div className="container mx-auto px-4 my-6 relative">
       {loading ? (
         <div className="container mx-auto px-4 my-6 relative ">
-          <h2 className="text-2xl font-semibold p-4 animate-pulse rounded-full bg-slate-200 block"></h2>
+          <h2 className="text-2xl font-semibold p-4 animate-pulse rounded-full bg-slate-200 dark:bg-gray-700 block"></h2>
         </div>
       ) : (
         <h2 className="text-2xl font-semibold py-4">{heading}</h2>
@@ -79,17 +79,17 @@ const VerticalCardProducts = ({ category, heading }) => {
               return (
                 <div
                   key={index}
-                  className="w-full min-w-[280px]  md:min-w-[320px] max-w-[280px] md:max-w-[320px]  bg-white rounded-sm shadow "
+                  className="w-full min-w-[280px]  md:min-w-[320px] max-w-[280px] md:max-w-[320px]  bg-white dark:bg-gray-800 rounded-sm shadow "
                 >
-                  <div className=" bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center animate-pulse"></div>
+                  <div className=" bg-slate-200 dark:bg-slate-600 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center animate-pulse"></div>
                   <div className="p-4 grid gap-3    ">
-                    <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black p-1 py-2 animate-pulse rounded-full bg-slate-200"></h2>
-                    <p className="capitalize text-slate-500 p-1 animate-pulse rounded-full bg-slate-200 py-2"></p>
+                    <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black p-1 py-2 animate-pulse rounded-full bg-slate-200 dark:bg-slate-600"></h2>
+                    <p className="capitalize text-slate-500 p-1 animate-pulse rounded-full bg-slate-200 dark:bg-slate-600 py-2"></p>
                     <div className="flex gap-3">
-                      <p className="text-red-600 font-medium p-1 animate-pulse rounded-full bg-slate-200 w-full py-2"></p>
-                      <p className="text-slate-500 line-through p-1 animate-pulse rounded-full bg-slate-200 w-full py-2"></p>
+                      <p className="text-red-600 font-medium p-1 animate-pulse rounded-full bg-slate-200 dark:bg-slate-600 w-full py-2"></p>
+                      <p className="text-slate-500 line-through p-1 animate-pulse rounded-full bg-slate-200 dark:bg-slate-600 w-full py-2"></p>
                     </div>
-                    <button className="text-sm px-3    animate-pulse rounded-full bg-slate-200 py-2"></button>
+                    <button className="text-sm px-3    animate-pulse rounded-full bg-slate-200 dark:bg-slate-600 py-2"></button>
                   </div>
                 </div>
               );
